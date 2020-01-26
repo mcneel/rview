115d6c653da0ade4de654c2fc8074479d2d62eb4
Steve Baer <steve@mcneel.com>
2020-01-26 11:21:25 -0800

    Support reading draco files

--- src/FileDraco.js
+++ src/FileDraco.js
@@ -1,34 +1,17 @@
 import RhinoApp from './RhinoApp.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 
 let FileDraco = {
   readFile (name, buffer) {
-    // let rhino3dm = RhinoApp.getRhino3dm()
-    let dracoLoader = new DRACOLoader()
-    dracoLoader.setDecoderConfig({ type: 'js' })
-    dracoLoader.setDecoderPath('./statics/')
-    dracoLoader.preload()
-
-    let taskConfig = {
-      attributeIDs: dracoLoader.defaultAttributeIDs,
-      attributeTypes: dracoLoader.defaultAttributeTypes,
-      useUniqueIDs: false
-    }
-    dracoLoader.decodeGeometry(buffer, taskConfig)
-      .then((geometry) => {
-        let rhino3dm = RhinoApp.getRhino3dm()
-        let doc = new rhino3dm.File3dm()
-        let layer = new rhino3dm.Layer()
-        layer.name = 'Default'
-        doc.layers().add(layer)
-        let attrs = new rhino3dm.ObjectAttributes()
-        attrs.layersIndex = 0
-        let json = geometry.toJSON()
-        let mesh = rhino3dm.Mesh.createFromThreejsJSON(json)
-        doc.objects().addMesh(mesh, attrs)
-        RhinoApp.setActiveDoc(name, doc)
-      })
-      .catch(dracoLoader.onError)
+    let rhino3dm = RhinoApp.getRhino3dm()
+    let mesh = rhino3dm.DracoCompression.decompressByteArray(buffer)
+    let doc = new rhino3dm.File3dm()
+    let layer = new rhino3dm.Layer()
+    layer.name = 'Default'
+    doc.layers().add(layer)
+    let attrs = new rhino3dm.ObjectAttributes()
+    attrs.layersIndex = 0
+    doc.objects().addMesh(mesh, attrs)
+    RhinoApp.setActiveDoc(name, doc)
   }
 }
 
