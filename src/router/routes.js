7bc7351705e437a18df12bab0c247c3d78d63fa4
Morteza Karimi <mkarimi@crh.io>
2020-07-02 19:11:30 -0400

    404 handling updated per V2 syntax

--- src/router/routes.js
+++ src/router/routes.js
@@ -11,11 +11,9 @@ const routes = [
 ]
 
 // Always leave this as last one
-if (process.env.MODE !== 'ssr') {
-  routes.push({
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  })
-}
+routes.push({
+  path: '*',
+  component: () => import('pages/Error404.vue')
+})
 
 export default routes
