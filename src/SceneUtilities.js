de553030ac43c872762a4ab21ea6bb54c4fbadf9
Morteza Karimi <mkarimi@crh.io>
2020-07-02 17:29:21 -0400

    Fixed bug with blocks bounding box

--- src/SceneUtilities.js
+++ src/SceneUtilities.js
@@ -247,12 +247,12 @@ let SceneUtilities = {
         {
           let parentId = geometry.parentIdefId
           let xf = geometry.xform.toFloatArray(true)
+          let bbox = null
           let group = new THREE.Group()
           let matrix = new THREE.Matrix4()
           matrix.set(xf[0], xf[1], xf[2], xf[3], xf[4], xf[5], xf[6], xf[7],
             xf[8], xf[9], xf[10], xf[11], xf[12], xf[13], xf[14], xf[15])
           group.applyMatrix(matrix)
-          objectsToAdd.push([group, null])
 
           let idefTable = doc.instanceDefinitions()
           let objectTable = doc.objects()
@@ -261,12 +261,22 @@ let SceneUtilities = {
           objectIds.forEach((id) => {
             let modelObject = objectTable.findId(id)
             let childGeometry = modelObject.geometry()
+            console.log('child geom:', childGeometry)
             let attr = modelObject.attributes()
             let children = this.createThreeGeometry(childGeometry, attr, doc)
+            console.log('children:', children)
             children.forEach((child) => {
+              let childBbox = child[1]
+              childBbox.transform(geometry.xform)
+              if (bbox == null) {
+                bbox = childBbox
+              } else {
+                bbox = rhino3dm.BoundingBox.union(bbox, childBbox)
+              }
               group.add(child[0])
             })
           })
+          objectsToAdd.push([group, bbox])
           objectTable.delete()
           idefTable.delete()
         }
