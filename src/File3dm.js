d64954ff3e792d6b00215173f762b0f5e27eff16
Morteza Karimi <mkarimi@crh.io>
2021-03-03 15:43:55 -0500

    Basic Support for layers

--- src/File3dm.js
+++ src/File3dm.js
@@ -10,14 +10,29 @@ let FilePly = {
     return new Promise((resolve, reject) => {
       loader.parse(contents, function (threeObj) {
         let doc = new rhino3dm.File3dm()
-        let layer = new rhino3dm.Layer()
-        layer.name = 'Default'
-        doc.layers().add(layer)
-        let attrs = new rhino3dm.ObjectAttributes()
-        attrs.layersIndex = 0
+        let defaultLayerIndex = null
+        const layers = threeObj.userData.layers
+
+        layers.forEach((l) => {
+          let layer = new rhino3dm.Layer()
+          layer.name = l.name
+          doc.layers().add(layer)
+        })
 
         threeObj.children.forEach((obj) => {
+          let attrs = new rhino3dm.ObjectAttributes()
           if (obj.type === 'Mesh') {
+            try {
+              attrs.layerIndex = obj.userData.attributes.layerIndex
+            } catch (error) {
+              if (!defaultLayerIndex) {
+                let layer = new rhino3dm.Layer()
+                layer.name = 'Default'
+                doc.layers().add(layer)
+                defaultLayerIndex = layers.length
+              }
+              attrs.layerIndex = defaultLayerIndex
+            }
             let mesh = rhino3dm.Mesh.createFromThreejsJSON(obj.geometry.toJSON())
             if (obj.material.color) {
               attrs.colorSource = rhino3dm.ObjectColorSource.ColorFromObject
